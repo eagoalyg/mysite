ec5f18365dfdbfc02f88a137ca4b53e83ad86c49
eagoalyg <1787024472@qq.com>
2019-12-20 20:53:56 +0800

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,13 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC+8'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = True 
 
 
 # Static files (CSS, JavaScript, Images)

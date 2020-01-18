20987dfe07f2032d6d179012f904163c976c38a7
eagoalyg <1787024472@qq.com>
2020-01-18 22:17:58 +0800

    basic website complete

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Blog, BlogType
 
 
+#主页视图
 def index(request):
     blogs = Blog.objects.all()
     blog_types = BlogType.objects.all()
@@ -9,12 +10,14 @@ def index(request):
     return render(request, 'blog/index.html', context)
 
 
+#博客页面视图
 def blog_detail(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
     context = {'blog': blog}
     return render(request, 'blog/blog_detail.html', context)
 
 
+#各分类博客视图
 def blog_classification(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     blogs = Blog.objects.filter(blog_type=blog_type)

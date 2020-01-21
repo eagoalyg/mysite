df9d862a6973dee72f2c8d0e9960b6b39ad83c90
eagoalyg <1787024472@qq.com>
2020-01-21 23:24:57 +0800

    html optimization

--- blog/views.py
+++ blog/views.py
@@ -13,14 +13,16 @@ def index(request):
 #博客页面视图
 def blog_detail(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
-    context = {'blog': blog}
+    blog_types = BlogType.objects.all()
+    context = {'blog': blog, 'blog_types':blog_types}
     return render(request, 'blog/blog_detail.html', context)
 
 
 #各分类博客视图
 def blog_classification(request, blog_type_pk):
+    blog_types = BlogType.objects.all()
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     blogs = Blog.objects.filter(blog_type=blog_type)
-    context = {'blog_type': blog_type, 'blogs': blogs}
+    context = {'blog_types': blog_types, 'blog_type': blog_type, 'blogs': blogs}
     return render(request, 'blog/blog_classification.html', context)
 
